e600ac4eeeab2637b78586a165f178d0eeebf3c6
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-07-28 19:31:46 +0200

    Add webpack.mix.js

---
+++ src/Frontend/webpack.mix.js
@@ -0,0 +1,30 @@
+const mix = require('laravel-mix');
+const mixPolyFill = require('laravel-mix-polyfill');
+
+// paths
+const srcPath  = './src';
+const distPath = './dist';
+
+mix.options({ manifest: false });
+
+mix.setPublicPath(distPath);
+
+// compile javascript
+mix.js(
+  `${srcPath}/main.js`,
+  'cmp-helper.js'
+);
+
+// transpile javascript
+mix.polyfill({
+  enabled: true,
+  useBuiltIns: "entry",
+  targets: "> 0.0001%"
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  mix.sourceMaps();
+  mix.webpackConfig({
+    devtool: 'inline-source-map'
+  });
+}
